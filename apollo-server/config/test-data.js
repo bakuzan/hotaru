00f85effc02154c821719c426a7625fb72c1aaf1
bakuzan <imannoyed4days@hotmail.co.uk>
2018-09-01 11:00:39 +0100

    added displayImages to test data.

--- apollo-server/config/test-data.js
+++ apollo-server/config/test-data.js
@@ -1,31 +1,104 @@
 const series = [
-  { name: 'Dagashi Kashi', source: 'Manga' },
-  { name: 'Witchcraft Works', source: 'Manga' },
-  { name: 'Prison School', source: 'Manga' },
-  { name: 'Kill La Kill', source: 'Anime' },
-  { name: 'Berserk', source: 'Manga' },
-  { name: 'Noragami', source: 'Manga' },
-  { name: 'Star Wars', source: 'Movie' }
+  {
+    name: 'Dagashi Kashi',
+    source: 'Manga',
+    displayImage: 'https://imgur.com/31pFAEr'
+  },
+  {
+    name: 'Witchcraft Works',
+    source: 'Manga',
+    displayImage: 'https://imgur.com/4z68zvZ'
+  },
+  {
+    name: 'Prison School',
+    source: 'Manga',
+    displayImage: 'https://imgur.com/Jc634zq'
+  },
+  {
+    name: 'Kill La Kill',
+    source: 'Anime',
+    displayImage: 'https://imgur.com/THrUQKx'
+  },
+  {
+    name: 'Berserk',
+    source: 'Manga',
+    displayImage: 'https://imgur.com/2jAkUoE'
+  },
+  {
+    name: 'Noragami',
+    source: 'Manga',
+    displayImage: 'https://imgur.com/6d9G2Ar'
+  },
+  {
+    name: 'Star Wars',
+    source: 'Movie',
+    displayImage: 'https://imgur.com/SeDH9Qm'
+  }
 ];
 const characters = [
-  { name: 'Shidare Hotaru', gender: 'Female' },
-  { name: 'Kagari Ayaka', gender: 'Female' },
-  { name: 'Shiraki Meiko', gender: 'Female' },
-  { name: 'Kurihara Mari', gender: 'Female' },
-  { name: 'Kiruin Satsuki', gender: 'Female' },
-  { name: 'Kiruin Ragyo', gender: 'Female' },
-  { name: 'Matoi Ryuuko', gender: 'Female' },
-  { name: 'Guts', gender: 'Male' },
-  { name: 'Joseph Joestar', gender: 'Male' },
-  { name: 'Darth Vader', gender: 'Male' },
-  { name: 'Bishamonten', gender: 'Female' }
+  {
+    name: 'Shidare Hotaru',
+    gender: 'Female',
+    displayImage: 'https://imgur.com/jLwdACg'
+  },
+  {
+    name: 'Kagari Ayaka',
+    gender: 'Female',
+    displayImage: 'https://imgur.com/3xdrczj'
+  },
+  {
+    name: 'Shiraki Meiko',
+    gender: 'Female',
+    displayImage: 'https://imgur.com/5hLv07N'
+  },
+  {
+    name: 'Kurihara Mari',
+    gender: 'Female',
+    displayImage: 'https://imgur.com/3BeLYsy'
+  },
+  {
+    name: 'Kiruin Satsuki',
+    gender: 'Female',
+    displayImage: 'https://imgur.com/hwpbszY'
+  },
+  {
+    name: 'Kiruin Ragyo',
+    gender: 'Female',
+    displayImage: 'https://imgur.com/xRWCpHW'
+  },
+  {
+    name: 'Matoi Ryuuko',
+    gender: 'Female',
+    displayImage: 'https://imgur.com/Gq8ulAp'
+  },
+  {
+    name: 'Guts',
+    gender: 'Male',
+    displayImage: 'https://imgur.com/0c3p5VT'
+  },
+  {
+    name: 'Joseph Joestar',
+    gender: 'Male',
+    displayImage: 'https://imgur.com/0jtjOQD'
+  },
+  {
+    name: 'Darth Vader',
+    gender: 'Male',
+    displayImage: 'https://imgur.com/EHFqL3C'
+  },
+  {
+    name: 'Bishamonten',
+    gender: 'Female',
+    displayImage: 'https://imgur.com/NTkNb9T'
+  }
 ];
 const tags = [
   { name: 'teenager' },
   { name: 'witch' },
   { name: 'student council' },
   { name: 'glasses' },
-  { name: 'thigh-highs' }
+  { name: 'thigh-highs' },
+  { name: 'mask' }
 ];
 
 module.exports = {
