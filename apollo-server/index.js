f07e4be4c8e29faec7dc98591eb44fd196416624
bakuzan <imannoyed4days@hotmail.co.uk>
2019-02-07 09:59:28 +0000

    upgrade apollo-server changes.

--- apollo-server/index.js
+++ apollo-server/index.js
@@ -1,10 +1,8 @@
 const path = require('path');
 const dotenv = require('dotenv');
-dotenv.config({ path: path.join(__dirname, '..', '.env.local') });
+dotenv.config();
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const { ApolloServer } = require('apollo-server-express');
 
 const Constants = require('./constants/index');
@@ -19,8 +17,12 @@ const server = new ApolloServer({
   context: () => ({
     ...context
   }),
+  introspection: true,
   playground: {
     settings: {
+      'editor.cursorShape': 'block',
+      'editor.fontSize': 16,
+      'editor.fontFamily': '"Lucida Console", Consolas, monospace',
       'editor.theme': 'light'
     }
   },
@@ -30,29 +32,19 @@ const server = new ApolloServer({
   }
 });
 
-const corsOptions = {
-  origin: function(origin, callback) {
-    if (Constants.whitelist.test(origin)) {
-      callback(null, true);
-    } else {
-      console.log(`Origin: ${origin}, not allowed by CORS`);
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
 // Overide origin if it doesn't exist
 app.use(function(req, _, next) {
   req.headers.origin = req.headers.origin || req.headers.host;
   next();
 });
-
 app.use(express.static(path.resolve(__dirname, '..', 'dist')));
-app.use('/graphql', cors(corsOptions), bodyParser.json());
 
 // Always return the main index.html, so react-router render the route in the client
 if (process.env.NODE_ENV === Constants.environment.production) {
-  app.get('*', (req, res) => {
+  app.get('*', (req, res, next) => {
+    if (req.url.includes('graphql')) {
+      next();
+    }
     res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
   });
 }
@@ -63,7 +55,19 @@ const PORT =
     ? process.env.PORT
     : process.env.SERVER_PORT) || 9006;
 
-server.applyMiddleware({ app });
+server.applyMiddleware({
+  app,
+  cors: {
+    origin: function(origin, callback) {
+      if (Constants.whitelist.test(origin)) {
+        callback(null, true);
+      } else {
+        console.log(`Origin: ${origin}, not allowed by CORS`);
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }
+});
 
 app.listen(PORT, () => {
   console.log(
